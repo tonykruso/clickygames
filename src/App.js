21b0cd4735941222423b54c0fff79607e7a5290d
Tony Kruso <tony.kruso@gmail.com>
2019-07-30 17:19:46 -0400

    everything works except the photos are HUGE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Matches from "./components/Matches";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+// import Container from "./components/Container";
+// import Row from "./components/Row";
 import cards from "./cards.json";
 import './App.css';
 
@@ -21,9 +23,9 @@ class App extends Component {
   };
 
   setClicked = id => {
-    const matches = this.state.matches;
+    const cards = this.state.cards;
     //filter for each clicked match
-    const clickedMatch = matches.filter(match => match.id === id);
+    const clickedMatch = cards.filter(match => match.id === id);
     //if else statements when pics are clicked. 
     if (clickedMatch[0].clicked){
       console.log("correct guesses: " + correctGuesses);
@@ -31,12 +33,12 @@ class App extends Component {
 
       correctGuesses = 0;
       clickMessage = "Dude, you already clicked on that one."
-      for (let i = 0; i < matches.length; i++){
-        matches[i].clicked = false;
+      for (let i = 0; i < cards.length; i++){
+        cards[i].clicked = false;
       }
       this.setState({ clickMessage });
       this.setState({ correctGuesses });
-      this.setState({ matches });
+      this.setState({ cards });
     } else if (correctGuesses < 11){
         clickedMatch[0].clicked = true;
         correctGuesses++;
@@ -46,8 +48,8 @@ class App extends Component {
             this.setState({ bestScore });
           }
           //shuffles cards when clicked
-          matches.sort(function(a, b){return 0.5 - Math.random()});
-          this.setState ({ matches });
+          cards.sort(function(a, b){return 0.5 - Math.random()});
+          this.setState ({ cards });
           this.setState ({ correctGuesses });
           this.setState ({ clickMessage });
     } else {
@@ -55,11 +57,11 @@ class App extends Component {
       clickMessage = "WHOA you actually got them all. Let's see if you can do it again!";
       bestScore = 12;
       this.setState({ bestScore });
-      for (let i=0; i < matches.length; i++){
-        matches[i].clicked = false;
+      for (let i=0; i < cards.length; i++){
+        cards[i].clicked = false;
       }
-      matches.sort(function(a, b){return 0.5 - Math.random()});
-      this.setState({ matches });
+      cards.sort(function(a, b){return 0.5 - Math.random()});
+      this.setState({ cards });
       this.setState({ correctGuesses });
       this.setState({ clickMessage });
     }
@@ -80,7 +82,7 @@ class App extends Component {
           </h3>
           <div className="container">
             <div className="row">
-              {this.state.matches.map(cards=> (
+              {this.state.cards.map(cards=> (
                 <Matches
                   setClicked={this.setClicked}
                   id={cards.id}
---
+++ src/components/Row/Row.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Row = props => <div className="row">{props.children.cards}</div>
+
+export default Row;
